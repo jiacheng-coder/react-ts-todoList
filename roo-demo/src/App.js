4a85a5437e61775d2583f7035d0a4ca221fd8362
zhujiacheng06 <zhujiacheng06@meituan.com>
2023-06-13 17:38:53 +0800

    Initial Commit

---
+++ roo-demo/src/App.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import Form from '@roo/roo/Form';
+import Button from '@roo/roo/Button';
+import Input from '@roo/roo/Input';
+import CheckBox from '@roo/roo/CheckBox';
+import '@roo/roo/theme/default/index.css';
+
+const Field = Form.field;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            password: '',
+        };
+    }
+
+    handleNameChange = (e) => {
+        this.setState({
+            name: e.target.value,
+        });
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value,
+        });
+    }
+
+    render() {
+        const { name, password } = this.state;
+        return (
+            <Form>
+                <Field label="用户名">
+                    <Input
+                        value={name}
+                        onChange={this.handleNameChange}
+                    />
+                </Field>
+                <Field label="密码">
+                    <Input
+                        type="password"
+                        value={password}
+                        onChange={this.handlePasswordChange}
+                    />
+                </Field>
+                <Field>
+                    <CheckBox>已同意相关政策和协议</CheckBox>
+                </Field>
+                <Field>
+                    <Button
+                        htmlType="submit"
+                        className="m-right-md"
+                    >
+                        确认
+                    </Button>
+                    <Button
+                        type="hollow"
+                        className="m-right-md"
+                    >
+                        取消
+                    </Button>
+                </Field>
+            </Form>
+        );
+    }
+}
+
+export default App;
