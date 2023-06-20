5780faabdff68cdc0151d02d718fe673adad346d
zhujiacheng06 <zhujiacheng06@meituan.com>
2023-06-20 10:23:28 +0800

    push

--- react-todo-mvc/src/components/Button/index.tsx
+++ react-todo-mvc/src/components/Button/index.tsx
@@ -1,10 +1,12 @@
-// import {Form,Button,Input,CheckBox} from '@roo/roo'
+import * as React from 'react';
 import Button from '@roo/roo/Button';
-// const Field = Form.field;
-// const CheckBoxGroup = CheckBox.Group;
 
-export default function Todos() {
-  return (
-    <Button>按钮</Button>
-  )
+class ButtonDemo extends React.Component {
+    render() {
+        return (
+            <Button>按钮</Button>
+        );
+    }
 }
+
+export default ButtonDemo;
